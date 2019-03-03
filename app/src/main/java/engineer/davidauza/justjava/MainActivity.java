748fd474f4483b93dc2165ab81f26c6dc8e29d4c
davidauza64 <davidauza@icloud.com>
2019-03-03 16:25:43 -0500

    Layout Version 2.

--- app/src/main/java/engineer/davidauza/justjava/MainActivity.java
+++ app/src/main/java/engineer/davidauza/justjava/MainActivity.java
@@ -26,9 +26,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         quantityTextView = findViewById(R.id.quantity_text_view);
-        displayQuantity(quantity);
         priceTextView = findViewById(R.id.price_text_view);
-        displayTotal(total);
+        updateScreen(quantity, total);
     }
 
     /**
@@ -39,48 +38,51 @@ public class MainActivity extends AppCompatActivity {
     }
 
     /**
-     * This method displays the given quantity value on the screen.
+     * This method updates the values of quantity and total in the screen
      */
-    private void displayQuantity(int number) {
-        quantityTextView.setText(Integer.toString(number));
+    private void updateScreen(int quantity, int total) {
+        quantityTextView.setText(Integer.toString(quantity));
+        priceTextView.setText(NumberFormat.getCurrencyInstance().format(total));
     }
 
     /**
-     * This method displays the given price on the screen.
-     */
-    private void displayTotal(int number) {
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
-
-    /**
-     * This method adds one to the number of coffees and displays it on the screen
+     * This method adds one to the number of coffees and displays it on the screen. It is called
+     * when the plus button is clicked.
      */
     public void increment(View view) {
         quantity++;
-        total = PRICE * quantity;
-        displayQuantity(quantity);
-        displayTotal(total);
+        updateTotal();
+        updateScreen(quantity, total);
     }
 
     /**
      * This method subtracts one to the number of coffees and displays it on the screen if the
-     * value of quantity is greater than one, else it shows a warning.
+     * value of quantity is greater than one, else it shows a warning. It is called when the minus
+     * button is clicked.
      */
     public void decrement(View view) {
         if (quantity > 1) {
             quantity--;
-            total = PRICE * quantity;
-            displayQuantity(quantity);
-            displayTotal(total);
+            updateTotal();
+            updateScreen(quantity, total);
         } else {
-            Toast toast = Toast.makeText(this, R.string.order_warning, Toast.LENGTH_SHORT);
+            Toast toast = Toast.makeText(this, R.string.order_warning,
+                    Toast.LENGTH_SHORT);
             if (Build.VERSION.SDK_INT >= 21) {
                 View viewToast = toast.getView();
-                viewToast.setBackgroundTintList(ColorStateList.valueOf(getResources().getColor(R.color.colorAccent)));
-                TextView text = (TextView) viewToast.findViewById(android.R.id.message);
+                viewToast.setBackgroundTintList(ColorStateList.valueOf(getResources().
+                        getColor(R.color.colorAccent)));
+                TextView text = viewToast.findViewById(android.R.id.message);
                 text.setTextColor(getResources().getColor(R.color.white));
             }
             toast.show();
         }
+    }
+
+    /**
+     * This method updates the total value
+     */
+    private void updateTotal() {
+        total = quantity * PRICE;
     }
 }
